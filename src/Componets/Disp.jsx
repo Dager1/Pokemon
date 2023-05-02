7468f63858f86411c726f0e5bfd7924ceef0126f
f <franklinrohan2002@gmail.com>
2023-05-02 20:50:35 +0530

    chore:Takes an input of pokemon name and displays it

--- src/Componets/Disp.jsx
+++ src/Componets/Disp.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Disp({ poki }) {
+function Disp({ pokemon }) {
   return (
     <div>
       <lu>
-        <li key={poki.name}>{poki.name}</li>
+        <li key={pokemon.name}>{pokemon.name}</li>
         <li>
           <img
-            ref={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poki.id}.png`}
+            ref={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
           />
         </li>
       </lu>
--- src/Componets/PokiInput.jsx
+++ src/Componets/PokiInput.jsx
@@ -1,29 +1,53 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Disp from "./Disp";
+import "./Style.css";
 
 function PokiInput() {
   const [name, setName] = useState("");
   const [poki, setPoki] = useState("");
 
+  console.log(name);
+
+  // ;
   useEffect(() => {
+    const pokeFeatch = async () => {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${name}`
+      );
+      console.log(name);
+      setPoki(response.data);
+    };
     if (name) {
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then((rep) => {
-        setPoki(rep.data);
-        console.log(rep.data);
-      });
+      pokeFeatch();
     }
   }, [name]);
-
   return (
     <div>
       <form>
         <div>
           <label>Enter the pokemon</label>
         </div>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value.toLowerCase())}
+        />
       </form>
-      <Disp poki={poki} />
+      {poki && (
+        <div>
+          <h1>{poki.name.toUpperCase()}</h1>
+          <img
+            src={poki.sprites.front_default}
+            alt={poki.name}
+            className="poster"
+          />
+          <div>Hp:{poki.stats[0].base_stat}</div>
+          <div>Attack:{poki.stats[1].base_stat}</div>
+          <div>Defence:{poki.stats[2].base_stat}</div>
+          <div>Special-attack:{poki.stats[3].base_stat}</div>
+          <div>Special-defence:{poki.stats[4].base_stat}</div>
+          <div>Speed:{poki.stats[5].base_stat}</div>
+        </div>
+      )}
     </div>
   );
 }
